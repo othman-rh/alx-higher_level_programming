6caa72782944225a90bd2cf98f4eb1845b8d9b4c
Othman Rhajbal <othman.rhajbal00@gmail.com>
2024-02-05 18:07:39 -0800

    python-inheritance

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python3
+"""Module Rectangle
+Creates a Rectangle class
+"""
+
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Type class of Rectangle inherit BaseGeometry"""
+    """Represents a rectangle
+    Private instance attributes:
+        - width
+        - height
+    Inherits from BaseGeometry
+    """
 
     def __init__(self, width, height):
+        """Initializes an instance
+
+        Args:
+            - width: rectangle width
+            - height: rectangle height
+        """
 
         self.integer_validator("width", width)
         self.integer_validator("height", height)
